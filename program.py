36a739b16a2278c146a2ab76eb3b724b7defd291
yashvantkotadiya <starlegend009@gmail.com>
2026-09-25 23:47:08 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,48 @@
+#1 print("Hello, World!")
+
+#2a = input("enter the character")
+
+#print(a)
+
+#3 a = int(input("enter the number : "))
+#print(a)
+
+#4 num = float(input("enter the number : "))
+#print(num)
+
+# 5 num1 = int(input("enter the first number : "))
+#num2 = int(input("enter the second number : "))  
+#print("addition  : ", num1 + num2)
+#print("subtraction  : ", num1 - num2)
+#print("multiplication  : ", num1 * num2)
+#print("division  : ", num1 / num2)
+
+#  6 num1 = int(input("enter the first number : "))
+#num2 = int(input("enter the second number : "))
+#quotient = num1 // num2
+#remainder = num1 % num2
+#print("Quotient : ", quotient)
+#print("Remainder : ", remainder)
+
+
+# 7 meter = float(input("Enter the distance in meters: "))
+#kilometers = meter / 1000
+#print("Distance in kilometers:", kilometers)
+
+#8 length = float(input("Enter the length in feet: "))
+#breadth = float(input("Enter the breadth in feet: "))
+#area = length * breadth
+#print("Area of the rectangle:", area)
+
+# 10length = float(input("Enter the length of the rectangle: "))
+#breadth = float(input("Enter the breadth of the rectangle: "))
+#height = float(input("Enter the height of the rectangle: "))
+#area = length * breadth*height
+#print("Area of the rectangle:", area)
+ 
+#10 base = float(input("enter base : "))
+#height = float(input("enter height : "))
+#area = (base* height)/2
+#print("area of a triangle : ",area)
+
+
